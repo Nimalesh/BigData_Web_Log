06f0eb0915b4387d82f89bbe7b0fdfd23dd5f8d6
Nimalesh <nimalesh123@gmail.com>
2025-06-12 20:49:57 +0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, Response
 
 app=Flask(__name__)
-@app.route('/app/log',methods=['GET'])
+@app.route('/log',methods=['GET'])
 def get_log():
     with open('/Users/nimalesh/Downloads/HPC/HPC.log', 'r') as file:
         log_content = file.read()
     return Response(log_content, mimetype='text/plain')
 if __name__ == '__main__':
     app.run(debug=True)
+
